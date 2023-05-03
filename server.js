9c26e433510f7629f7273b363af768df0cada238
gamartins013 <gamartins013@gmail.com>
2023-05-02 22:16:22 -0300

    fix: arrumando porta

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { dirname } from 'path';
 import router from './router.js';
 
 const app = express();
-const PORT = process.env.PORT || 3070;
+const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
