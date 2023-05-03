0f37a2b575f55310b802f2eedd7be2de1ab0aad7
gamartins013 <gamartins013@gmail.com>
2023-05-03 20:35:54 -0300

    fix: porta

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { dirname } from 'path';
 import router from './router.js';
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 443;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
