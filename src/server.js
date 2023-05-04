0efb36cd2ada0f57029130164c384f2960bb7a35
gamartins013 <gamartins013@gmail.com>
2023-05-03 21:09:42 -0300

    fix:corrigindo porta e path para renderização de view

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 import express from 'express';
-import session from 'express-session';
-import bodyParser from 'body-parser';
-import passport from 'passport';
-import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import passport from 'passport';
 import router from './router.js';
+import path from 'path';
+
 
 const app = express();
 
-const PORT = process.env.PORT || 443;
-
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(__dirname, 'project', 'views'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({ secret: 'your-session-secret', resave: false, saveUninitialized: false }));
