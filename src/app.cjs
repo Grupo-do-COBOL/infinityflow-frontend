523952f9c408df68a6589e94e71517722723c387
GuiFernoli <gui.supernova@gmail.com>
2023-06-05 10:12:30 -0300

    subindo att local para repo

--- src/app.cjs
+++ src/app.cjs
@@ -1,19 +1,15 @@
 const express = require('express');
 const path = require('path');
-const bcrypt = require('bcrypt');
 const axios = require('axios');
 const session = require('express-session');
+const _ = require('lodash');  // Adicione essa linha para incluir a biblioteca lodash
 const app = express();
-const {
-  LocalStorage
-} = require('node-localstorage');
-const localStorage = new LocalStorage('./localStorage');
-
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'project', 'views'));
 
@@ -40,33 +36,102 @@ app.use(session({
   saveUninitialized: true
 }));
 
+// Função de sanitização
+function sanitizeData(data) {
+  return _.transform(data, function(result, value, key) {
+    if (!_.isFunction(value)) {
+      result[key] = value;
+    }
+  });
+}
 
-
-async function authenticateUser(username, password) {
+async function authenticate(email, senha) {
   try {
     const response = await axios.post('http://191.101.71.67:8080/api/v1/login/autenticacao', {
-      email: username,
-      senha: password,
+      email,
+      senha,
     });
 
-    if (response.status === 200 && response.data.token) {
-      token = response.data.token;
-
-
-      return response.data;
-    } else {
-      throw new Error('Authentication failed');
-    }
+    return response.data.token;
   } catch (error) {
-    console.error('Error during authentication:', error);
-
-    if (error.response && error.response.status === 401) {
-      throw new Error('Invalid username or password');
-    } else {
-      throw new Error('Authentication failed');
-    }
+    console.error('Erro na autenticação:', error);
+    return null;
   }
 }
+
+async function getUserDataByEmail(email, token) {
+  try {
+    const response = await axios.get('http://191.101.71.67:8080/api/v1/usuarios/buscar_por_email', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      params: {
+        email
+      }
+    });
+
+
+
+    const userData = response.data;
+    console.log("userData:", userData);  // Adicionado console.log para verificar o valor de userData
+    console.log('Token de Acesso: ', token);
+    return userData;
+  } catch (error) {
+    console.error('Erro ao buscar dados do usuário:', error);
+    return null;
+  }
+}
+
+async function getAulasList(token, userId) {
+  try {
+      const response = await axios.get('http://191.101.71.67:8080/sistema/v1/lista_aulas', {
+          headers: {
+              'Authorization': `Bearer ${token}`
+          },
+          params: {
+              idProfessor: userId
+          }
+      });
+
+      if (!response.data || response.data.length === 0) {
+        console.log('Não há aulas hoje');
+          // Retorna um objeto especial quando não há aulas
+          return 'Não há aulas hoje';
+          } else {
+            console.log('Dados Aulas: ', response.data);
+            return response.data;
+          }
+    } catch (error) {
+      console.error(error);
+  }
+}
+
+async function getAlunosList(token, id_aula) {
+  try {
+    const response = await axios.get('http://191.101.71.67:8080/sistema/v1/lista_alunos', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      params: {
+        idAula:id_aula
+      }
+    });
+
+    if (!response.data || response.data.length === 0) {
+      console.log('Não há alunos na lista');
+      // Retorna um array vazio quando não há alunos
+      return ['Não há alunos na lista'];
+    } else {
+      console.log('Dados Alunos: ', response.data);
+      return response.data;
+    }
+  } catch (error) {
+    console.error('Erro ao buscar lista de alunos:', error);
+    return [];
+  }
+}
+
+
 
 
 app.get('/', (req, res) => {
@@ -86,15 +151,22 @@ app.post('/login', async (req, res) => {
     email,
     senha
   } = req.body;
-  localStorage.setItem('email', email);
-  console.log("email app.cjs: " + email);
+
   try {
-    const authData = await authenticateUser(email, senha);
-    req.session.authData = authData;
+    const token = await authenticate(email, senha);
 
-    localStorage.setItem('token', authData.token);
+    if (token) {
+      req.session.authData = {
+        email: email,
+        token: token,
+      };
 
-    res.redirect('/mainpage');
+      res.redirect('/mainpage');
+    } else {
+      res.status(401).render('login', {
+        errorMessage: 'Invalid username or password'
+      });
+    }
   } catch (error) {
     res.status(401).render('login', {
       errorMessage: 'Invalid username or password'
@@ -102,22 +174,114 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.get('/mainpage', (req, res) => {
+
+app.get('/mainpage', async (req, res) => {
   if (req.session.authData) {
-    res.render('mainpage', {
-      user: req.session.authData,
-      email: req.session.authData.email, // Adicione esta linha
-      token: req.session.authData.token, // Adicione esta linha
-    });
+    const { email, token } = req.session.authData;
+    const selectedAulaId = req.session.selectedAulaId;
+    
+    if (!token) {
+      console.error('Token inválido:', token);
+      return res.status(401).send('Token inválido');
+    }
+    
+    const userData = await getUserDataByEmail(email, token);
+    
+    if (!userData || !userData.professor_id) {
+      console.error('Dados de usuário inválidos:', userData);
+      return res.status(500).send('Não foi possível obter os dados do usuário');
+    }
+
+    try {
+      const userId = userData.professor_id;
+      let aulas = await getAulasList(token, userId);
+      let alunos = [];
+
+      if (selectedAulaId) {
+        alunos = await getAlunosList(token, selectedAulaId);
+      }
+      
+      // Verifica se `aulas` é um array
+      if (Array.isArray(aulas)) {
+        const aulasFiltradas = aulas.filter(aula => !aula.confirmacao);
+
+        res.render('mainpage', {
+          userInfo: userData,
+          aulas: aulasFiltradas,
+          alunos
+        });
+      } else {
+        console.error('Aulas não é um array:', aulas);
+        aulas = [];
+        res.render('mainpage', {
+          userInfo: userData,
+          aulas,
+          alunos
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao buscar as aulas:", error);
+      res.status(500).send(`Erro ao buscar as aulas: ${error.message}`);
+    }
+
   } else {
     res.redirect('/login');
   }
 });
 
+
+
 app.post('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/login');
 });
+
+app.post('/setSelectedAula', async (req, res) => {
+  const id_aula = req.body.id_aula;
+  req.session.selectedAulaId = id_aula;
+
+  if (req.session.authData) {
+    const { token } = req.session.authData;
+
+    try {
+      const alunos = await getAlunosList(token, id_aula);
+      res.json(alunos);
+    } catch (error) {
+      console.error('Erro ao buscar lista de alunos:', error);
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.post('/sistema/v1/registrar_presencas', async (req, res) => {
+  if (!req.session.authData) {
+    res.sendStatus(401); // Unauthorized
+    return;
+  }
+
+  const { token } = req.session.authData;
+  
+  // Extrair os dados do corpo da requisição
+  const { id_aula, lista_presencas } = req.body;
+
+  // Aqui, você irá processar os dados recebidos, como registrá-los em um banco de dados
+ 
+
+  // Por exemplo, você pode querer iterar sobre lista_presencas e registrar cada presença
+  lista_presencas.forEach(presenca => {
+    console.log(`Registrando presença para o aluno ${presenca.id_aluno} na aula ${id_aula}. Situação: ${presenca.situacao}`);
+    // ... adicione aqui o código para registrar a presença no banco de dados
+  });
+
+  // Em seguida, responda à solicitação para informar ao cliente que ela foi bem-sucedida
+  res.status(200).json({ message: `Presença dos alunos registrada com sucesso para a aula ${id_aula}.` });
+});
+
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/project/js/scripts.js
+++ src/project/js/scripts.js
@@ -1,92 +1,180 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Initialize the welcomeModal
-  var welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+function getJsonDataFromScriptTag(scriptId) {
+  const scriptTag = document.getElementById(scriptId);
+  if (!scriptTag) {
+    console.error('Script tag not found:', scriptId);
+    return null;
+  }
+  const jsonData = scriptTag.textContent;
+  try {
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error('Failed to parse JSON from script tag', error);
+    return null;
+  }
+}
 
-  welcomeModal.show();
 
-  // Add this event listener to call fetchUserData() when the modal is shown
-  document.getElementById('welcomeModal').addEventListener('shown.bs.modal', function () {
-    fetchUserData();
+function populateAulaSelect() {
+  const aulaSelect = document.getElementById('aulaSelect');
+  const data = getJsonDataFromScriptTag('userAndAulasData');
+
+  // Se data.aulas é uma string, adicione essa string como uma opção e retorne
+  if (typeof data.aulas === 'string') {
+    const option = document.createElement('option');
+    option.value = '';
+    option.textContent = 'Não há aulas hoje';
+    aulaSelect.appendChild(option);
+    return;
+  }
+
+  // Caso contrário, assuma que data.aulas é um array e adicione cada aula como uma opção
+  const aulas = Array.isArray(data.aulas) ? data.aulas : [];
+  aulas.forEach(aula => {
+    const option = document.createElement('option');
+    option.value = aula.id_aula;
+    option.textContent = `${aula.materia}, ${aula.ano_letivo}ª série, Turma ${aula.turma}, ${aula.periodo}`;
+    aulaSelect.appendChild(option);
   });
 
-  const startClassBtn = document.getElementById('startClassBtn');
-  if (startClassBtn) {
-    startClassBtn.addEventListener('click', function () {
-      const selectedClass = document.querySelector('.form-select').value;
-      if (selectedClass !== "Escolha uma aula") {
-        const attendanceTab = document.getElementById('attendance-tab');
-        attendanceTab.classList.add('active');
-        const welcomeModal = bootstrap.Modal.getInstance(document.getElementById('welcomeModal'));
-        welcomeModal.hide();
-      } else {
-        alert('Por favor, selecione uma aula.');
-      }
+  // Adicione isso dentro do manipulador 'change' do aulaSelect
+  aulaSelect.addEventListener('change', function () {
+    const selectedAulaId = this.value;
+
+    axios.post('/setSelectedAula', {
+        id_aula: selectedAulaId
+      })
+      .then(function (response) {
+        // Adicione esta lógica
+        // Limpa a tabela de alunos atual
+        const tableBody = document.querySelector('#attendance tbody');
+        tableBody.innerHTML = '';
+
+        // Adiciona novos alunos à tabela
+        const alunos = response.data || [];
+        alunos.forEach(aluno => {
+          const row = document.createElement('tr');
+
+          const nameCell = document.createElement('td');
+          nameCell.textContent = aluno.nome;
+          row.appendChild(nameCell);
+
+          const attendanceCell = document.createElement('td');
+          const checkbox = document.createElement('input');
+          checkbox.type = 'checkbox';
+          checkbox.id = `${aluno.id}`;
+          attendanceCell.appendChild(checkbox);
+          row.appendChild(attendanceCell);
+
+          tableBody.appendChild(row);
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+
+}
+
+// Request para registrar presença dos alunos
+document.getElementById("registrarPresenca").addEventListener('click', function () {
+  // Obter todas as linhas da tabela de presenças
+  const rows = document.querySelectorAll('#attendance tbody tr');
+
+  // Criar uma lista vazia para armazenar os registros de presença
+  let lista_presencas = [];
+
+  // Iterar por cada linha
+  for (let i = 0; i < rows.length; i++) {
+    // Obter o ID do aluno a partir do ID do checkbox
+    const checkbox = rows[i].querySelector('input[type=checkbox]');
+    const alunoId = checkbox.id;
+
+    // Determinar o status de presença com base na marcação do checkbox
+    const presenca = checkbox.checked ? 'P' : 'A';
+
+    // Adicionar o registro de presença à lista
+    lista_presencas.push({
+      id_aluno: alunoId,
+      situacao: presenca
     });
   }
 
-  async function fetchUserData() {
-    const email = localStorage.getItem('email');
-    const token = localStorage.getItem('token');
-  
-    console.log('Email e token obtidos do localStorage:', email, token); // Adicione esta linha
-  
-    if (email && token) {
-      try {
-        const response = await fetch(`http://191.101.71.67:8080/api/v1/usuarios/buscar_por_email?email=${email}`, {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-          },
-        });
-  
-        console.log('Resposta da API:', response); // Adicione esta linha
-  
-        if (response.ok) {
-          const data = await response.json();
-          console.log('Dados da API:', data); // Adicione esta linha
-          document.getElementById('welcomeModalLabel').innerHTML = 'Bem-vindo, ' + data.nome_usuario + '!';
-          localStorage.setItem('ProfessorID', data.professor_id);
-          console.log('ID do professor salvo no localStorage:', data.professor_id);
-        } else {
-          throw new Error('Error fetching user name');
-        }
-      } catch (error) {
-        console.error('Error during fetching user name:', error);
-      }
-    }
-  
-    fetchAulas();
-  }
-  
-  function fetchAulas() {
-    const aulaSelect = document.getElementById('aulaSelect');
-    const IdProfessor = localStorage.getItem('ProfessorID');
-    const token = localStorage.getItem('token');
-    const url = `http://191.101.71.67:8080/sistema/v1/lista_aulas?idProfessor=${IdProfessor}`;
+  console.log('Dados de Presença: ', lista_presencas);
 
-    fetch(url, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Erro ao buscar a lista de aulas');
-        }
-        return response.json();
-      })
-      .then(data => {
-        data.forEach(aula => {
-          if (!aula.confirmacao) {
-            const option = document.createElement('option');
-            option.value = aula.id_aula;
-            option.text = `${aula.materia}, ${aula.ano_letivo}ª série, Turma ${aula.turma}, Período: ${aula.periodo}`;
-            aulaSelect.add(option);
-          }
-        });
-      })
-      .catch(error => {
-        console.error('Erro ao buscar a lista de aulas:', error);
-      });
+  // Obter o ID da aula selecionada
+  const selectedAulaId = document.getElementById('aulaSelect').value;
+
+  // Agora, faça a solicitação POST com a lista completa de presenças
+  axios.post('http://191.101.71.67:8080/sistema/v1/registrar_presencas', {
+      id_aula: selectedAulaId,
+      lista_presencas: lista_presencas
+  })
+  .then(function (response) {
+      // Manipule a resposta aqui, se necessário
+      console.log(response.data);
+      alert('Presenças registradas com sucesso!');
+  })
+  .catch(function (error) {
+      // Manipule o erro aqui, se necessário
+      console.log(error);
+      alert('Presenças registradas com sucesso!');
+  });
+});
+
+
+
+
+// Função para exibir a modal de boas-vindas com o select de aulas
+function showOptionModal() {
+
+  if (localStorage.getItem('isRedirectedFromModal') === 'true') {
+    localStorage.removeItem('isRedirectedFromModal');
+    return; // Não mostre a modal
+  } else if (localStorage.getItem('optionModalShown') === 'true') {
+    localStorage.removeItem('optionModalShown');
+    return;
   }
+
+  const data = getJsonDataFromScriptTag('userAndAulasData');
+  const userInfo = data ? data.userInfo : {};
+  const userName = document.querySelector('#optionModalLabel');
+  userName.textContent = `Bem-vindo, ${userInfo.nome_usuario}!`;
+
+  const optionModal = new bootstrap.Modal(document.getElementById('optionModal'));
+  optionModal.show();
+
+
+}
+
+
+// Função para exibir a modal de boas-vindas com o select de aulas
+function showWelcomeModal() {
+  const data = getJsonDataFromScriptTag('userAndAulasData');
+  const userInfo = data ? data.userInfo : {};
+  const userName = document.querySelector('#welcomeModalLabel');
+  userName.textContent = `${userInfo.nome_usuario}`;
+
+  populateAulaSelect();
+
+  const welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+  welcomeModal.show();
+}
+
+
+// Adiciona eventos de clique para o botão para redirecionar para a mainpage e gerar relatórios.
+document.getElementById('openReports').addEventListener('click', function () {
+
+  localStorage.setItem('isRedirectedFromModal', 'true');
+  localStorage.setItem('optionModalShown', 'true');
+  $('#optionModal').modal('hide');
+  // Redirecionar para a página de relatórios
+  window.location.href = "/mainpage";
+
 
 });
+
+// Chama a função showOptionModal quando o DOM estiver carregado para abrir a modal com o que o professor gostaria de fazer 
+document.addEventListener('DOMContentLoaded', showOptionModal);
+
+// Adiciona eventos de clique para o botão para exibir a modal com as aulas do dia
+document.getElementById('openWelcomeModal').addEventListener('click', showWelcomeModal);
